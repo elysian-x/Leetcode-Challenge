16491f0b3e50f87921666e03d2854cce7205c520
elysian-x <nehaannphilip24@gmail.com>
2026-03-20 16:19:32 +0530

    Create Check_equal_arrays.py

---
+++ Check_equal_arrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        s1,s2="",""
+        for i in word1:
+            s1+=i
+        for i in word2:
+            s2+=i
+        if s1==s2:
+            return True
+        else:
+            return False
+        
